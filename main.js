c16411e8ae3734170dfdb1c939b2d4c020e319e2
Neya <neya@tmps-Mac-mini-2.local>
2025-12-16 18:46:24 -0800

    update

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+const title = document.getElementById("title");
+const story = document.getElementById("story");
+const essay = document.getElementById("essay");
+const page_img = document.getElementById("page_img");
+front = true;
 page() {
-	console.log("clicked")
+	if front {
+		page_img.src = "";
+		front = false;
+	} else {
+		page_img.src = "";
+		front = true;
+	}
 }
